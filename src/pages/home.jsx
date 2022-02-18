53efc60099b4d88f50a5c8565294d6f7e46f5d52
Glitch (glitch-blank-react) <none>
2022-02-18 04:45:03 +0000

    🎿🚇 Checkpoint ./.glitch-assets:42362062/342 ./src/pages/home.jsx:42362062/305 ./src/styles/styles.css:42362062/195

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import { useState } from 'react';
 
 export default function IloPiSitelenPona() {
-  const [linja, ] = "Hello React!";
+  const [input, setInput] = useState("toki a!");
 
   return (
     <>
       <h1 className="title">sina ken pali e sitelen pona</h1>
-      <textarea value></textarea>
+      <textarea class="input" value={input} onChange={ev => setInput(ev.target.value)}></textarea>
+      <h3>~~~~~~~~~~~~~~~~~</h3>
+      <p></p>
     </>
   );
 }
