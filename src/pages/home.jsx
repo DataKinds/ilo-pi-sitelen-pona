3f96054c7d3182698b807547b8323822545d3e57
Glitch (glitch-blank-react) <none>
2022-02-18 05:09:03 +0000

    🍽🍜 Checkpoint ./src/pages/home.jsx:42362062/455 ./src/styles/styles.css:42362062/69

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,24 @@
 import * as React from "react";
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export default function IloPiSitelenPona() {
   const [input, setInput] = useState("toki a!");
+  
+  const tokenize = useCallback((tpString) => {
+    const toks = tpString.split(/(\d)/)
+    return toks
+  }, [])
 
   return (
     <>
       <h1 className="title">sina ken pali e sitelen pona</h1>
-      <h3>Write your toki pona here:</h3>
-      <h3>o pali e toki lon toki pona </h3>
+      <div class="row">
+        <h3 style={{marginRight: 2 + 'em'}}>Write your toki pona here:</h3>
+        <h3>o sitelen e toki lon toki pona:</h3>
+      </div>
       <textarea class="input" value={input} onChange={ev => setInput(ev.target.value)}></textarea>
-      <h3>Copy your sitelen pony from here:</h3>
-      <p class="output"></p>
+      <h3>Copy your sitelen pona from here:</h3>
+      <p class="output">{JSON.stringify(tokenize(input))}</p>
     </>
   );
 }
