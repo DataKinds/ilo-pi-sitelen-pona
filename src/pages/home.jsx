75b5db4bfe60fc50657ae58ffe454247e2d2131c
Glitch (glitch-blank-react) <none>
2022-02-18 06:20:03 +0000

    📕🏟 Checkpoint ./src/pages/home.jsx:42362062/193

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -159,13 +159,20 @@ export default function IloPiSitelenPona() {
   return (
     <>
       <h1 className="title">sina ken pali e sitelen pona</h1>
+      <div>
+        A few instructions // kepeken nasin lili
+        <ul>
+          <li>
+          </li>
+        </ul>
+      </div>
       <div class="row">
         <h3 style={{marginRight: 2 + 'em'}}>Write your toki pona here:</h3>
         <h3>o sitelen e toki lon toki pona:</h3>
       </div>
       <textarea class="input" value={input} onChange={ev => setInput(ev.target.value)}></textarea>
       <h3>Copy your sitelen pona from here:</h3>
-      <p class="output">{JSON.stringify(tokens)}<br/>{output}</p>
+      <p class="output">{output}</p>
       <p class="credits">
         Font from <a href="https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml">https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml</a>.
       </p>
