9822727521e64c87faa4d25ebad4fd892fb95d6b
Glitch (glitch-blank-react) <none>
2025-12-31 16:42:15 -0700

    Fixup mulTIline strings

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -145,6 +145,148 @@ const TOKI_TABLE = {
   "APEJA": String.fromCodePoint(0xF19A1),
   "MAJUNA": String.fromCodePoint(0xF19A2),
   "POWE": String.fromCodePoint(0xF19A3),
+  [String.fromCodePoint(0xF1900)]: "a",
+  [String.fromCodePoint(0xF1901)]: "akesi",
+  [String.fromCodePoint(0xF1902)]: "ala",
+  [String.fromCodePoint(0xF1903)]: "alasa",
+  [String.fromCodePoint(0xF1904)]: "ali",
+  [String.fromCodePoint(0xF1904)]: "ale",
+  [String.fromCodePoint(0xF1905)]: "anpa",
+  [String.fromCodePoint(0xF1906)]: "ante",
+  [String.fromCodePoint(0xF1907)]: "anu",
+  [String.fromCodePoint(0xF1908)]: "awen",
+  [String.fromCodePoint(0xF1909)]: "e",
+  [String.fromCodePoint(0xF190A)]: "en",
+  [String.fromCodePoint(0xF190B)]: "esun",
+  [String.fromCodePoint(0xF190C)]: "ijo",
+  [String.fromCodePoint(0xF190D)]: "ike",
+  [String.fromCodePoint(0xF190E)]: "ilo",
+  [String.fromCodePoint(0xF190F)]: "insa",
+  [String.fromCodePoint(0xF1910)]: "jaki",
+  [String.fromCodePoint(0xF1911)]: "jan",
+  [String.fromCodePoint(0xF1912)]: "jelo",
+  [String.fromCodePoint(0xF1913)]: "jo",
+  [String.fromCodePoint(0xF1914)]: "kala",
+  [String.fromCodePoint(0xF1915)]: "kalama",
+  [String.fromCodePoint(0xF1916)]: "kama",
+  [String.fromCodePoint(0xF1917)]: "kasi",
+  [String.fromCodePoint(0xF1918)]: "ken",
+  [String.fromCodePoint(0xF1919)]: "kepeken",
+  [String.fromCodePoint(0xF191A)]: "kili",
+  [String.fromCodePoint(0xF191B)]: "kiwen",
+  [String.fromCodePoint(0xF191C)]: "ko",
+  [String.fromCodePoint(0xF191D)]: "kon",
+  [String.fromCodePoint(0xF191E)]: "kule",
+  [String.fromCodePoint(0xF191F)]: "kulupu",
+  [String.fromCodePoint(0xF1920)]: "kute",
+  [String.fromCodePoint(0xF1921)]: "la",
+  [String.fromCodePoint(0xF1922)]: "lape",
+  [String.fromCodePoint(0xF1923)]: "laso",
+  [String.fromCodePoint(0xF1924)]: "lawa",
+  [String.fromCodePoint(0xF1925)]: "len",
+  [String.fromCodePoint(0xF1926)]: "lete",
+  [String.fromCodePoint(0xF1927)]: "li",
+  [String.fromCodePoint(0xF1928)]: "lili",
+  [String.fromCodePoint(0xF1929)]: "linja",
+  [String.fromCodePoint(0xF192A)]: "lipu",
+  [String.fromCodePoint(0xF192B)]: "loje",
+  [String.fromCodePoint(0xF192C)]: "lon",
+  [String.fromCodePoint(0xF192D)]: "luka",
+  [String.fromCodePoint(0xF192E)]: "lukin",
+  [String.fromCodePoint(0xF192F)]: "lupa",
+  [String.fromCodePoint(0xF1930)]: "ma",
+  [String.fromCodePoint(0xF1931)]: "mama",
+  [String.fromCodePoint(0xF1932)]: "mani",
+  [String.fromCodePoint(0xF1933)]: "meli",
+  [String.fromCodePoint(0xF1934)]: "mi",
+  [String.fromCodePoint(0xF1935)]: "mije",
+  [String.fromCodePoint(0xF1936)]: "moku",
+  [String.fromCodePoint(0xF1937)]: "moli",
+  [String.fromCodePoint(0xF1938)]: "monsi",
+  [String.fromCodePoint(0xF1939)]: "mu",
+  [String.fromCodePoint(0xF193A)]: "mun",
+  [String.fromCodePoint(0xF193B)]: "musi",
+  [String.fromCodePoint(0xF193C)]: "mute",
+  [String.fromCodePoint(0xF193D)]: "nanpa",
+  [String.fromCodePoint(0xF193E)]: "nasa",
+  [String.fromCodePoint(0xF193F)]: "nasin",
+  [String.fromCodePoint(0xF1940)]: "nena",
+  [String.fromCodePoint(0xF1941)]: "ni",
+  [String.fromCodePoint(0xF1942)]: "nimi",
+  [String.fromCodePoint(0xF1943)]: "noka",
+  [String.fromCodePoint(0xF1944)]: "o",
+  [String.fromCodePoint(0xF1945)]: "olin",
+  [String.fromCodePoint(0xF1946)]: "ona",
+  [String.fromCodePoint(0xF1947)]: "open",
+  [String.fromCodePoint(0xF1948)]: "pakala",
+  [String.fromCodePoint(0xF1949)]: "pali",
+  [String.fromCodePoint(0xF194A)]: "palisa",
+  [String.fromCodePoint(0xF194B)]: "pan",
+  [String.fromCodePoint(0xF194C)]: "pana",
+  [String.fromCodePoint(0xF194D)]: "pi",
+  [String.fromCodePoint(0xF194E)]: "pilin",
+  [String.fromCodePoint(0xF194F)]: "pimeja",
+  [String.fromCodePoint(0xF1950)]: "pini",
+  [String.fromCodePoint(0xF1951)]: "pipi",
+  [String.fromCodePoint(0xF1952)]: "poka",
+  [String.fromCodePoint(0xF1953)]: "poki",
+  [String.fromCodePoint(0xF1954)]: "pona",
+  [String.fromCodePoint(0xF1955)]: "pu",
+  [String.fromCodePoint(0xF1956)]: "sama",
+  [String.fromCodePoint(0xF1957)]: "seli",
+  [String.fromCodePoint(0xF1958)]: "selo",
+  [String.fromCodePoint(0xF1959)]: "seme",
+  [String.fromCodePoint(0xF195A)]: "sewi",
+  [String.fromCodePoint(0xF195B)]: "sijelo",
+  [String.fromCodePoint(0xF195C)]: "sike",
+  [String.fromCodePoint(0xF195D)]: "sin",
+  [String.fromCodePoint(0xF195E)]: "sina",
+  [String.fromCodePoint(0xF195F)]: "sinpin",
+  [String.fromCodePoint(0xF1960)]: "sitelen",
+  [String.fromCodePoint(0xF1961)]: "sona",
+  [String.fromCodePoint(0xF1962)]: "soweli",
+  [String.fromCodePoint(0xF1963)]: "suli",
+  [String.fromCodePoint(0xF1964)]: "suno",
+  [String.fromCodePoint(0xF1965)]: "supa",
+  [String.fromCodePoint(0xF1966)]: "suwi",
+  [String.fromCodePoint(0xF1967)]: "tan",
+  [String.fromCodePoint(0xF1968)]: "taso",
+  [String.fromCodePoint(0xF1969)]: "tawa",
+  [String.fromCodePoint(0xF196A)]: "telo",
+  [String.fromCodePoint(0xF196B)]: "tenpo",
+  [String.fromCodePoint(0xF196C)]: "toki",
+  [String.fromCodePoint(0xF196D)]: "tomo",
+  [String.fromCodePoint(0xF196E)]: "tu",
+  [String.fromCodePoint(0xF196F)]: "unpa",
+  [String.fromCodePoint(0xF1970)]: "uta",
+  [String.fromCodePoint(0xF1971)]: "utala",
+  [String.fromCodePoint(0xF1972)]: "walo",
+  [String.fromCodePoint(0xF1973)]: "wan",
+  [String.fromCodePoint(0xF1974)]: "waso",
+  [String.fromCodePoint(0xF1975)]: "wawa",
+  [String.fromCodePoint(0xF1976)]: "weka",
+  [String.fromCodePoint(0xF1977)]: "wile",
+  [String.fromCodePoint(0xF1978)]: "namako",
+  [String.fromCodePoint(0xF1979)]: "kin",
+  [String.fromCodePoint(0xF197A)]: "oko",
+  [String.fromCodePoint(0xF197B)]: "kipisi",
+  [String.fromCodePoint(0xF197C)]: "leko",
+  [String.fromCodePoint(0xF197D)]: "monsuta",
+  [String.fromCodePoint(0xF197E)]: "tonsi",
+  [String.fromCodePoint(0xF197F)]: "jasima",
+  [String.fromCodePoint(0xF1980)]: "kijetesantakalu",
+  [String.fromCodePoint(0xF1981)]: "soko",
+  [String.fromCodePoint(0xF1982)]: "meso",
+  [String.fromCodePoint(0xF1983)]: "epiku",
+  [String.fromCodePoint(0xF1984)]: "kokosila",
+  [String.fromCodePoint(0xF1985)]: "lanpan",
+  [String.fromCodePoint(0xF1986)]: "n",
+  [String.fromCodePoint(0xF1987)]: "misikeke",
+  [String.fromCodePoint(0xF1988)]: "ku",
+  [String.fromCodePoint(0xF19A0)]: "pake",
+  [String.fromCodePoint(0xF19A1)]: "apeja",
+  [String.fromCodePoint(0xF19A2)]: "majuna",
+  [String.fromCodePoint(0xF19A3)]: "powe",
   "[": "BEGIN_LONG_PI",
   "]": "END_LONG_PI",
   "{": "BEGIN_PROPER_NOUN",
@@ -164,11 +306,11 @@ const LEX_TABLE = {
 
 export default function IloPiSitelenPona() {
   const [input, setInput] = useState("toki a!");
-  
-  const tokens = useMemo(() => input.split(/(\W|\[|\]|\{|\})/).filter(word => word.trim().length > 0).map(word => word.toUpperCase()), [input])
-  
+                                                                                           
+  const tokens = useMemo(() => input.split(/([\u{F1900}-\u{F19A3}])/u).flatMap(i => i.split(/(\W+|\[|\]|\{|\})/u)).filter(word => word != " ").map(word => word.toUpperCase()), [input])
+  const lexes = useMemo(() => tokens.map(tok => TOKI_TABLE[tok] || tok), [tokens])
+
   const outputList = useMemo(() => {
-    const lexes = tokens.map(tok => TOKI_TABLE[tok] || tok);
     const out = [];
     let inLongPi = false;
     let inProperNoun = false;
@@ -197,7 +339,7 @@ export default function IloPiSitelenPona() {
       out.push(LEX_TABLE[lex] === undefined ? lex : LEX_TABLE[lex])
     }
     return out;
-  }, [tokens])
+  }, [lexes])
   
   const output = useMemo(() => outputList.join("").toLowerCase(), [outputList])
   
@@ -242,36 +384,43 @@ export default function IloPiSitelenPona() {
         <h3>poki anpa li pana e sitelen pona sina:</h3>
       </div>
       {showCopiedPopup ? (
-        <p class="output row justify-between copied-popup">
-          <div class="col-grow">
+        <div>
+          <p class="output w-100 copied-popup">
             Copied!
-          </div>
+          </p>
           <div class="col-auto">
             <button class="copy-button" onClick={copyToClipboard}>Copy to clipboard!</button>
           </div>
-        </p>
+        </div>
       ) : (
-        <p class="output row justify-between">
-          <div class="col-grow">
-            {output}
-          </div>
+        <div>
+          <p class="output w-100">
+              {output}
+          </p>
           <div class="col-auto">
             <button class="copy-button" onClick={copyToClipboard}>Copy to clipboard!</button>
           </div>
-        </p>
+        </div>
       )}
       <footer>
         <p class="credits">
           Font from <a href="https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml">https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml</a>.
+          // nasin sitelen tan <a href="https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml">https://www.kreativekorp.com/software/fonts/fairfaxhd.shtml</a>.
         </p>
         <p class="credits">
           Unicode proposal at <a href="https://www.kreativekorp.com/ucsur/charts/sitelen.html">https://www.kreativekorp.com/ucsur/charts/sitelen.html</a>, tracked at <a href="https://www.kreativekorp.com/ucsur/">https://www.kreativekorp.com/ucsur/</a>.
         </p>
         <p class="credits">
-          Read more about this project (including installation instructions for all your apps) in the guide at <a href="https://github.com/Id405/sitelen-pona-ucsur-guide/blob/main/README.md">https://github.com/Id405/sitelen-pona-ucsur-guide/blob/main/README.md</a>.
+          Read installation instructions for writing in sitelen pona on your computer here: <a href="https://github.com/neroist/sitelen-pona-ucsur-guide/blob/main/README.md">https://github.com/neroist/sitelen-pona-ucsur-guide/blob/main/README.md</a>. 
         </p>
         <p class="credits">
-          FOLLOW ME ON TWITTER <a href="https://twitter.com/DataKinds">@DataKinds</a> and read my blog :3 <a href="https://datakinds.github.io">https://datakinds.github.io</a>
+          See more info about <a href="https://sona.pona.la/wiki/sitelen_pona">sitelen pona</a> and <a href="https://sona.pona.la/wiki/Fonts">other fonts</a>.
+        </p>
+        <p class="credits">
+          See my other projects at <a href="https://github.com/DataKinds">https://github.com/DataKinds</a> and read my blog :3 <a href="https://datakinds.github.io">https://datakinds.github.io</a>
+        </p>
+        <p class="credits">
+          Contribute to this project at <a href="https://github.com/DataKinds/ilo-pi-sitelen-pona">https://github.com/DataKinds/ilo-pi-sitelen-pona</a>
         </p>
       </footer>
     </>
